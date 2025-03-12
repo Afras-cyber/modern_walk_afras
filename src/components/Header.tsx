fb14af40b4ad512b98f1fc2cb91a37aa0e79bc01
Ahamed Afras <54540692+Afras-cyber@users.noreply.github.com>
2025-03-12 19:07:31 +0530

    refactor: update project structure and styling for Modern Walk

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center py-4 ">
+      {/* need to add shadow */}
+      <h1 className="text-3xl font-bold text-center">Modern Walk</h1>
+    </div>
+  );
+};
+export default Header;
